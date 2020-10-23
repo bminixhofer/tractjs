5aa8705cb6deaca6f13bfaefcbd27c5355c2178c
Github Action <action@github.com>
2020-10-23 07:51:17 +0000

    Deploying to gh-pages from  @ 3909ef101e8ea5fdb4d510fd515eaa7ec0b4c21c 🚀

--- dist/options.d.ts
+++ dist/options.d.ts
@@ -1,6 +1,14 @@
 import type { DataType } from "tractjs-core";
+declare type SymbolValues = {
+    [id: string]: number;
+};
+declare type DimSymbol = string | {
+    id: string;
+    slope: number;
+    intercept: number;
+};
 declare type Format = "onnx" | "tensorflow";
-declare type Shape = Array<number>;
+declare type Shape = Array<number | DimSymbol>;
 declare type Fact = [DataType, Shape];
 /**
  * Model loading options.
@@ -15,15 +23,15 @@ declare type Options = {
      */
     optimize?: boolean;
     /**
-     * The node names of model inputs. Passed to [`set_input_names`](https://docs.rs/tract-core/0.9.2/tract_core/model/struct.ModelImpl.html#method.set_input_names).
+     * The node names of model inputs. Passed to [`set_input_names`](https://docs.rs/tract-core/0.11.1/tract_core/model/struct.ModelImpl.html#method.set_input_names).
      */
     inputs?: Array<string>;
     /**
-     * The node names of model outputs. Passed to [`set_output_names`](https://docs.rs/tract-core/0.9.2/tract_core/model/struct.ModelImpl.html#method.set_output_names).
+     * The node names of model outputs. Passed to [`set_output_names`](https://docs.rs/tract-core/0.11.1/tract_core/model/struct.ModelImpl.html#method.set_output_names).
      */
     outputs?: Array<string>;
     /**
-     * Mapping of indices to facts to set for the input. Each fact is passed to [`set_input_fact`](https://docs.rs/tract-core/0.9.2/tract_core/model/struct.ModelImpl.html#method.set_input_fact).
+     * Mapping of indices to facts to set for the input. Each fact is passed to [`set_input_fact`](https://docs.rs/tract-core/0.11.1/tract_core/model/struct.ModelImpl.html#method.set_input_fact).
      */
     inputFacts?: Record<number, Fact>;
 };
@@ -34,4 +42,4 @@ declare type InternalOptions = {
     outputs?: Array<string>;
     inputFacts: Record<number, Fact>;
 };
-export { Format, Options, InternalOptions };
+export { Format, Options, InternalOptions, SymbolValues };
--- dist/tractjs.d.ts
+++ dist/tractjs.d.ts
@@ -14,8 +14,16 @@ export declare class Tensor {
 	shape: number[];
 	constructor(data: TypedArray, shape: number[]);
 }
+export declare type SymbolValues = {
+	[id: string]: number;
+};
+export declare type DimSymbol = string | {
+	id: string;
+	slope: number;
+	intercept: number;
+};
 export declare type Format = "onnx" | "tensorflow";
-export declare type Shape = Array<number>;
+export declare type Shape = Array<number | DimSymbol>;
 export declare type Fact = [DataType, Shape];
 /**
  * Model loading options.
@@ -30,15 +38,15 @@ export declare type Options = {
 	 */
 	optimize?: boolean;
 	/**
-	 * The node names of model inputs. Passed to [`set_input_names`](https://docs.rs/tract-core/0.9.2/tract_core/model/struct.ModelImpl.html#method.set_input_names).
+	 * The node names of model inputs. Passed to [`set_input_names`](https://docs.rs/tract-core/0.11.1/tract_core/model/struct.ModelImpl.html#method.set_input_names).
 	 */
 	inputs?: Array<string>;
 	/**
-	 * The node names of model outputs. Passed to [`set_output_names`](https://docs.rs/tract-core/0.9.2/tract_core/model/struct.ModelImpl.html#method.set_output_names).
+	 * The node names of model outputs. Passed to [`set_output_names`](https://docs.rs/tract-core/0.11.1/tract_core/model/struct.ModelImpl.html#method.set_output_names).
 	 */
 	outputs?: Array<string>;
 	/**
-	 * Mapping of indices to facts to set for the input. Each fact is passed to [`set_input_fact`](https://docs.rs/tract-core/0.9.2/tract_core/model/struct.ModelImpl.html#method.set_input_fact).
+	 * Mapping of indices to facts to set for the input. Each fact is passed to [`set_input_fact`](https://docs.rs/tract-core/0.11.1/tract_core/model/struct.ModelImpl.html#method.set_input_fact).
 	 */
 	inputFacts?: Record<number, Fact>;
 };
@@ -60,18 +68,20 @@ export declare class Model {
 	/**
 	 * Runs the model on the given input.
 	 * @param inputs - List of input tensors.
+	 * @param symbolValues - (optional) values for symbolic dimensions.
 	 *
 	 * @returns Promise for a list of output tensors.
 	 */
-	predict(inputs: Tensor[]): Promise<Tensor[]>;
+	predict(inputs: Tensor[], symbolValues?: SymbolValues): Promise<Tensor[]>;
 	/**
 	 * Runs the model on a single input tensor.
 	 * This method is provided as convenience method for interfacing with Rust WASM, since arrays of custom objects are not supported yet.
 	 * @param input - a single input tensor.
+	 * @param symbolValues - (optional) values for symbolic dimensions.
 	 *
 	 * @returns The first output tensor.
 	 */
-	predict_one(input: Tensor): Promise<Tensor>;
+	predict_one(input: Tensor, symbolValues?: SymbolValues): Promise<Tensor>;
 	/**
 	 * Removes all references to the internal model allowing it to be garbage collected.
 	 */
